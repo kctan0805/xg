40e9e99cb077fce944cba10cccd6754f2c271173
Jim Tan <KuoChun.Tan@ite.com.tw>
2019-12-04 15:16:20 +0800

    fix cannot create solution file bug

--- build_vs2017.cmd
+++ build_vs2017.cmd
@@ -1,4 +1,3 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\WDExpress\Common7\Tools\VsDevCmd.bat"
-git submodule update --init
 cmake -H. -B./build -G"Visual Studio 15 2017 Win64"
 @pause
