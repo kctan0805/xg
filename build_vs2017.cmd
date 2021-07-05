6a58b0ca8f22e6b3b35528d3e4235a6e311146ae
Jim Tan <KuoChun.Tan@ite.com.tw>
2021-07-05 14:15:02 +0800

    update submodule link; disable openxr as default

--- build_vs2017.cmd
+++ build_vs2017.cmd
@@ -1,4 +1,3 @@
-git -C third_party/KTX-Software lfs pull
 call "D:\Program Files (x86)\Microsoft Visual Studio\2017\WDExpress\Common7\Tools\VsDevCmd.bat"
 cmake -H. -B./build -G"Visual Studio 15 2017 Win64"
 @pause
--- build_vs2019.cmd
+++ build_vs2019.cmd
@@ -1,2 +1,2 @@
-cmake -H. -B./build -G"Visual Studio 16 2019" -A x64
+cmake -H. -B./build -G"Visual Studio 16 2019" -A x64 -DXG_ENABLE_REALITY=ON
 @pause
