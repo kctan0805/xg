1fe67abc4b75b3b19056eaa8675596e82fbdb9a7
kctan0805 <kctan_tw@hotmail.com>
2021-11-21 11:06:10 +0800

    change vs2019 to vs2022

--- build_vs2019.cmd
+++
@@ -1,2 +0,0 @@
-cmake -H. -B./build -G"Visual Studio 16 2019" -A x64 -DXG_ENABLE_REALITY=ON
-@pause
---
+++ build_vs2022.cmd
@@ -0,0 +1,2 @@
+cmake -H. -B./build -G"Visual Studio 17 2022" -A x64 -DXG_ENABLE_REALITY=ON
+@pause
